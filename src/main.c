0c83b8127261d639b0d3cfd361990a7ea39a869e
RandomProgrammerOnTheInternet <125991130+RandomProgrammerOnTheInternet@users.noreply.github.com>
2025-05-28 19:45:17 -0400

    big rewrite

--- src/main.c
+++ src/main.c
@@ -1,79 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-#include <ctype.h>
 #include <stdint.h>
-
-#include "util.h"
+#define PP_IMPL
+#include "pp.h"
+#define LEXER_IMPL
 #include "lexer.h"
 
-void parse(token_t *);
-//FILE *generate(node_t *);
-void compile(int, char **);
-//node_t *node_push_back(node_t *, size_t);
+typedef enum error_code : u16 {
+	no_file,
+	invalid_file
+} error_code;
+
+void error(error_code code, char *message);
+char *read_whole_file(FILE *f);
+void compile(int argc, char **argv);
 
 int main(int argc, char **argv) {
-    if(argc == 1) {
-        printf("skill issue: you gotta put a file\n");
-        exit(1);
-    }
-    FILE *file = NULL;
-    if(!(file = fopen(argv[1], "r"))) {
-        printf("skill issue: invalid file\n");
-        exit(1);
-    }
-    char *str = read_whole_file(file);
-    token_t *tokens = tokenize(str);
-    //node_t *nodes = parse(tokens);
-    //FILE *output = generate(nodes);
-    //fclose(output);
-    //system("nasm -felf64 out.asm");
-    //system("ld -o a.out out.o");
-    for(int i = 0; i < token_amount; i++) {
-        printf("Token %d: ", i);
-        printf("%s\n", tokens[i].value);
-    }
-    printf("-------------------------------\nAmount of nodes: NULL\n");
-    printf("-------------------------------\n");
+	compile(argc, argv);
 
-    for(int i = 0; i < token_amount; i++) {
-        printf("%d\n", tokens[i].type);
-    }
-    for(int i = 0; i < token_amount; i++) {
-        safe_free(tokens[i].value);
-    }
-    safe_free(tokens);
-    safe_free(str);
-    //safe_free(nodes);
-    fclose(file);
-
-    return 0;
+	return 0;
 }
 
-/*
-FILE *generate(node_t *nodes) {
-    FILE *f = fopen("out.asm", "w+");
-    fprintf(f, "global _start\n");
-    fprintf(f, "_start:\n");
-    for(int i = 0; i < node_amount; i++) {
-        if(nodes[i].tag == node_return) {
-            fprintf(f, "    mov rax, 60\n");
-            i++;
-            nodes[i].tag = node_expr;
-            fprintf(f, "    mov rdi, %s\n", nodes[i + 1].int_lit.token.value);
-            i++;
-            nodes[i].tag = node_int_lit;
-            fprintf(f, "    syscall\n");
-        }
-    }
+void error(u16 code, char *message) {
+	fprintf(stderr, "skill issue #%hu: ", code);
+	fprintf(stderr, "%s\n", message);
+	exit(1);
+}
 
-    return f;
+char *read_whole_file(FILE *f) {
+	  fseek(f, 0, SEEK_END);
+	  long fsize = ftell(f);
+	  fseek(f, 0, SEEK_SET);
+	  char *str = malloc(fsize + 1);
+	  fread(str, fsize, 1, f);
+	  str[fsize] = 0;
+
+	  return str;
 }
-*/
-/*
-node_t *node_push_back(node_t *node, size_t size) {
-    node = (node_t *)realloc(node, size * sizeof(node_t));
-    return node;
+
+void compile(int argc, char **argv) {
+	if(argc == 1) {
+		error(no_file, "you need a file bro");
+	}
+	FILE *src_file = NULL;
+	if(!(src_file = fopen(argv[1], "r"))) {
+		error(invalid_file, "invalid file");
+	}
+	char *src_str = preprocess(read_whole_file(src_file));
+	printf("%s\n", src_str);
+	LIST(token_t) tokens = tokenize(src_str);
+	printf("%lu\n", tokens.size);
+	for(int i = 0; i < tokens.size / sizeof(tokens); i++) {
+		printf("tokens.value[%d].value: %s\n", i, tokens.value[i].value);
+		printf("tokens.value[%d].type:  %d\n", i, tokens.value[i].type);
+		free(tokens.value[i].value);
+	}
+	free(src_str);
+	fclose(src_file);
+	LIST_FREE(tokens);
 }
-*/
