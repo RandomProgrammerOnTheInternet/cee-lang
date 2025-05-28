4219441ff269c3294ef7190c553fa02897107f7e
RandomProgrammerOnTheInternet <125991130+RandomProgrammerOnTheInternet@users.noreply.github.com>
2025-05-28 19:46:15 -0400

    big rewrite

--- src/util.h
+++ src/util.h
@@ -1,11 +1,30 @@
 #ifndef UTIL_H
 #define UTIL_H
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-char *read_whole_file(FILE*);
-char *char_push_back(char*, char, size_t);
-void safe_free(void*);
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
 
-#endif // CEE_UTIL_H
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef float f32;
+typedef double f64;
+
+char *substr(char *str, u64 start, u64 end);
+
+#endif // UTIL_H
+
+#ifdef UTIL_IMPL
+
+char *substr(char *str, u64 start, u64 end) {
+	return strndup(str + start, end - start);
+}
+
+#endif // UTIL_IMPL
