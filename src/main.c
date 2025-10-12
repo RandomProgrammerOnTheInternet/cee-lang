0fe42d8ebd0328ce1f9f0ab7846389ac9d3ca2a9
RandomProgrammerOnTheInternet <125991130+RandomProgrammerOnTheInternet@users.noreply.github.com>
2025-10-12 15:49:05 -0400

    yay parser works

--- src/main.c
+++ src/main.c
@@ -7,6 +7,8 @@
 #include "lexer.h"
 #define PARSER_IMPL
 #include "parser.h"
+#define CODEGEN_IMPL
+#include "codegen.h"
 #define UTIL_IMPL
 #include "util.h"
 
@@ -15,7 +17,7 @@ void compile(char **argv);
 
 int main(int argc, char **argv) {
 	if(argc == 1) {
-		error(no_file, "you need a file bro");
+		error(error_no_file, "you need a file bro");
 	}
 	compile(argv);
 
@@ -36,14 +38,23 @@ char *read_whole_file(FILE *f) {
 void compile(char **argv) {
 	FILE *src_file = NULL;
 	if(!(src_file = fopen(argv[1], "r"))) {
-		error(invalid_file, "invalid file");
+		error(error_invalid_file, "invalid file");
 	}
 	char *src_str = read_whole_file(src_file);
 	printf("Preprocessed File:\n%s\n", src_str);
 	LIST(token_t) tokens = tokenize(src_str);
-	printf("%lu\n", tokens.size);
+	printf("tokens.length = %lu\n", tokens.length);
+	node_base_t base_node = parse(tokens);
+	printf("base_node.return_node.int_lit.token.value %s\n", base_node.return_node.int_lit_node.token.value);
+	char *asm_code = generate_asm(base_node);
+	FILE *asm_file = fopen("out.asm", "w");
+	fprintf(asm_file, "%s", asm_code);
+	fclose(asm_file);
+	free(asm_code);
+	system("as -o out.o out.asm && ld out.o");
 
-	for(int i = 0; i < tokens.size / sizeof(tokens); i++) {
+	printf("tokens: ");
+	for(int i = 0; i < tokens.length; i++) {
 		printf("%s ", tokens.value[i].value);
 		free(tokens.value[i].value);
 	}
