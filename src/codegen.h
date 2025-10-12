23778f089ea354a5b112e019e45b3a6dc99b03f1
RandomProgrammerOnTheInternet <125991130+RandomProgrammerOnTheInternet@users.noreply.github.com>
2025-10-12 15:50:28 -0400

    it compiles!

---
+++ src/codegen.h
@@ -0,0 +1,30 @@
+#ifndef CODEGEN_H
+#define CODEGEN_H
+
+#include <stdio.h>
+#include <string.h>
+#include "ds.h"
+
+NEW_LIST(char);
+
+int asprintf(char **restrict strp, const char *restrict fmt, ...); // ...
+char *generate_asm(node_base_t node);
+
+#endif // CODEGEN_H
+
+#ifdef CODEGEN_IMPL
+
+char *generate_asm(node_base_t node) {
+	char* str;
+	asprintf(&str, ".global _start\n"
+				  "_start:\n"
+				  "mov $60, %%rax\n"
+				  "mov $%s, %%rdi\n"
+				  "syscall\n",
+				  node.return_node.int_lit_node.token.value);
+
+	
+	return str;
+}
+
+#endif // CODEGEN_IMPL
