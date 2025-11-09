865f5bb236e359aaf2e9b5c90c6f680c6be3843c
RandomProgrammerOnTheInternet <125991130+RandomProgrammerOnTheInternet@users.noreply.github.com>
2025-11-09 18:56:48 -0500

    variables

--- src/codegen.h
+++ src/codegen.h
@@ -1,29 +1,57 @@
-#ifndef CODEGEN_H
+f CODEGEN_H
 #define CODEGEN_H
 
 #include <stdio.h>
 #include <string.h>
 #include "ds.h"
 
-int asprintf(char **restrict strp, const char *restrict fmt, ...); // ...
-char *generate_asm(LIST(node_base_t) node);
+FILE *generate_asm(LIST(node_base_t) node);
+void generate_return(LIST(node_base_t) node, FILE **file, size_t *i);
+void generate_var_declaration(LIST(node_base_t) node, FILE **file, size_t *i);
 
 #endif // CODEGEN_H
 
 #ifdef CODEGEN_IMPL
 
-char *generate_asm(LIST(node_base_t) node) {
-	char* str;
-	asprintf(&str, ".intel_syntax noprefix\n"
-				   ".global _start\n"
-				   "_start:\n"
-				   "	mov rax, 60\n"
-				   "	mov rdi, %s\n"
-				   "	syscall\n",
-				   node.value[0].statement_node.return_node.expression_node.int_lit_node.token.value);
+FILE *generate_asm(LIST(node_base_t) node) {
+	FILE* file = fopen("out.asm", "a");
+	fprintf(file, ".intel_syntax noprefix\n"
+				  ".global _start\n"
+				  "_start:\n");
+	for(size_t i = 0; i < node.length; i++) {
+		switch(node.value[i].statement_node.type) {
+		case node_return:
+			generate_return(node, &file, &i);
+			break;
+		case node_var_declaration:
+			generate_var_declaration(node, &file, &i);
+			break;
+		}
+	}
 	LIST_FREE(node);
 	
-	return str;
+	return file;
+}
+
+void generate_return(LIST(node_base_t) node, FILE **file, size_t *i) {
+	printf("called generate_return()\n");
+	fprintf(*file, "	mov rax, 60\n");
+	if(node.value[*i].statement_node.return_node.expression_node.type == node_int_lit) {
+		fprintf(*file, "	mov edi, %s\n", node.value[*i].statement_node.return_node.expression_node.int_lit_node.token.value);
+	}
+	else {
+		fprintf(*file, "	mov edi, dword ptr [rsp-%zu]\n", node.value[*i].statement_node.return_node.expression_node.var_node.stack_offset);
+	}
+
+	fprintf(*file, "	syscall\n");
+}
+
+void generate_var_declaration(LIST(node_base_t) node, FILE **file, size_t *i) {
+	if(!node.value[*i].statement_node.var_declaration_node.has_value) {
+		return;
+	}
+	fprintf(*file, "	mov dword ptr [rsp-%zu], %s\n", node.value[*i].statement_node.var_declaration_node.stack_offset,
+													   node.value[*i].statement_node.var_declaration_node.expression_node.int_lit_node.token.value);
 }
 
 #endif // CODEGEN_IMPL
