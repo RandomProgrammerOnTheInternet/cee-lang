f5f983a4ac092a8ddd94a24052c01ffabddc95a1
RandomProgrammerOnTheInternet <125991130+RandomProgrammerOnTheInternet@users.noreply.github.com>
2025-11-09 18:56:25 -0500

    fix

--- src/ds.h
+++ src/ds.h
@@ -4,17 +4,17 @@
 #include <stdlib.h>
 
 // Dynamic Arrays
-#define NEW_LIST(x) struct list_ ## x {x* value; size_t length; size_t test[7];}
+#define NEW_LIST(x) struct list_ ## x {x* value; size_t length;}
 
 #define LIST(x) struct list_ ## x
 
 #define INIT_LIST(x, y) \
 x.length = y; \
-x.value = malloc(x.length * sizeof(typeof(x)));
+x.value = malloc(x.length * sizeof(typeof(*x.value)))
 
 #define LIST_RESIZE(x, y) \
 x.length = y; \
-x.value = realloc(x.value, x.length * sizeof(typeof(x)))
+x.value = realloc(x.value, x.length * sizeof(typeof(*x.value)))
 
 #define LIST_ADD(x, y) LIST_RESIZE(x, x.length + y)
 
