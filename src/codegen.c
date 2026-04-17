adc05ca44a6a3c216dffec1635c23cd5dcece23f
RandomProgrammerOnTheInternet <125991130+RandomProgrammerOnTheInternet@users.noreply.github.com>
2026-04-17 16:15:13 -0400

    logs

--- src/codegen.c
+++ src/codegen.c
@@ -3,14 +3,17 @@
 #include "codegen_arm64.h"
 
 FILE *generate_asm(LIST(node_base_t) node, enum backend backend) {
+    LOG(PRN_YLW, "called generate_asm");
     switch(backend){
         case backend_x86:
             return generate_asm_x86(node);
         case backend_arm64:
             return generate_asm_arm64(node);
         default:
+            LOG(PRN_YLW, "ERROR");
             return NULL;
-    }    
+    }
+    LOG(PRN_YLW, "ERROR");
 
     return NULL;
 }
