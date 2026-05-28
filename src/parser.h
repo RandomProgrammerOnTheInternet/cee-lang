bd0f6903a21eef0272559f1fcbdb95e33084a070
RandomProgrammerOnTheInternet <abdulmohsin.aldhalimi@gmail.com>
2026-05-28 14:38:35 -0400

    refactor expressions

--- src/parser.h
+++ src/parser.h
@@ -18,7 +18,9 @@ typedef enum node_type : u8 {
 	node_label,
 	node_goto,
 	node_assignment,
-	node_bin_expr
+	node_prim_expr,
+	node_mul_expr,
+	node_add_expr
 } node_type;
 
 typedef enum op_type : u8 {
@@ -29,65 +31,80 @@ typedef enum op_type : u8 {
 	op_mod
 } op_type;
 
-static const u8 op_precedence[] = {
-	2, // op_add
-	2, // op_sub
-	1, // op_mul
-	1, // op_div
-	1  // op_mod
-};
-
-typedef struct node_int_lit_t {
+typedef struct node_int_lit {
 	token_t token;
 } node_int_lit_t;
 
-typedef struct node_var_t {
+typedef struct node_var {
 	size_t stack_offset;
 	token_t token;
 } node_var_t;
 
-struct node_bin_expr_t;
-typedef struct node_bin_expr_t node_bin_expr_t;
-
-typedef struct node_expr_t {
+typedef struct node_prim_expr {
 	node_type type;
 	union {
 		node_int_lit_t *int_lit_node;
 		node_var_t var_node;
-		node_bin_expr_t *bin_expr_node;
+	};
+} node_prim_expr_t;
+
+typedef struct node_add_expr node_add_expr_t;
+
+typedef struct node_expr {
+	node_type type;
+	union {
+		node_add_expr_t *add_expr_node;
 	};
 } node_expr_t;
 
-struct node_bin_expr_t {
-	op_type op;
-	node_expr_t *lhs;
-	node_expr_t *rhs;
-};
+typedef struct node_mul_expr {
+	node_type type;
+	union {
+		node_prim_expr_t *prim_expr_node;
+		struct {
+			op_type op;
+			node_mul_expr_t *lhs;
+			node_prim_expr_t *rhs;
+		};
+	};
+} node_mul_expr_t;
 
-typedef struct node_var_decl_t {
+typedef struct node_add_expr {
+	node_type type;
+	union {
+		node_mul_expr_t mul_expr_node;
+		struct {
+			op_type op;
+			node_add_expr_t *lhs;
+			node_mul_expr_t *rhs;
+		};
+	};
+} node_add_expr_t;
+
+typedef struct node_var_decl {
 	token_t token;
 	size_t stack_offset;
 	node_expr_t *expr_node;
 } node_var_decl_t;
 
-typedef struct node_assignment_t {
+typedef struct node_assignment {
 	node_var_t lhs;
 	node_expr_t *rhs;
 } node_assignment_t;
 
-typedef struct node_return_t {
+typedef struct node_return {
 	node_expr_t *expr_node;
 } node_return_t;
 
-typedef struct node_label_t {
+typedef struct node_label {
 	token_t token;
 } node_label_t;
 
-typedef struct node_goto_t {
+typedef struct node_goto {
 	token_t token;
 } node_goto_t;
 
-typedef struct node_statement_t {
+typedef struct node_statement {
 	node_type type;
 	union {
 		node_var_decl_t *var_decl_node;
@@ -98,7 +115,7 @@ typedef struct node_statement_t {
 	};
 } node_statement_t;
 
-typedef struct node_base_t {
+typedef struct node_base {
 	node_type type;
 	union {
 		node_statement_t *statement_node;
